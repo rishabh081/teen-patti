8a68f15215d58cee043ba6cbcdd1e6b8af9fd2a1
rishabh081 <rishabh.1js14cs081@gmail.com>
2021-02-12 00:45:19 +0530

    Code refactor.

--- src/main/java/com/teen/patti/Main.java
+++ src/main/java/com/teen/patti/Main.java
@@ -5,12 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 
 		PokerTable pt = new PokerTable();
-
 		pt.addPlayers("Akshay");
 		pt.addPlayers("Mayank");
 		pt.addPlayers("Jay");
 		pt.addPlayers("Amit");
-
 		pt.setPlayerCard();
 		pt.cardIdentify();
 	}
